763119a0062178384c82181c45a4256d5741ec64
Liam Cottam <killtheidols@hotmail.co.uk>
2017-04-04 06:41:00 +0100

    Use suggestion from other repo: https://github.com/xSke/Pxls/pull/5

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ module.exports = {
   boardFilename: 'board.dat',
   boardFilenameTemp: 'board.tmp.dat',
   clearColor: 0,
-  palette: ["#FFFFFF", "#E4E4E4", "#888888", "#222222", "#FFA7D1", "#E50000", "#E59500", "#A06A42", "#E5D900", "#94E044", "#02BE01", "#00D3DD", "#0083C7", "#0000EA", "#CF6EE4", "#820080"],
+  palette: ["#DEEED6", "#DAD45E", "#6DC2CA", "#D2AA99", "#6DAA2C", "#8595A1", "#D27D2C", "#597DCE", "#757161", "#D04648", "#346524", "#854C30", "#4E4A4F", "#30346D", "#442434", "#140C1C"],
 };
