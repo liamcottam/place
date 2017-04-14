051195fc5624767e74d556191efc59d2a70efc55
Liam Cottam <killtheidols@hotmail.co.uk>
2017-04-14 19:08:36 +0100

    Remove unused route

--- routes/index.js
+++ routes/index.js
@@ -21,24 +21,6 @@ function Router(app) {
     res.send(new Buffer(app.boardData));
   });
 
-  router.get('/cooldown', function (request, res, next) {
-    var ip = app.formatIP(request.headers['x-forwarded-for'] ||
-      request.connection.remoteAddress ||
-      request.socket.remoteAddress ||
-      request.connection.socket.remoteAddress);
-
-    if (typeof app.ipClients[ip] !== 'undefined') {
-      var diff = (app.ipClients[ip].cooldown - Date.now()) / 1000;
-      if (diff <= 0) {
-        res.json(0);
-      } else {
-        res.json(Math.ceil(diff));
-      }
-    } else {
-      res.json(0);
-    }
-  });
-
   router.get('/restricted', (req, res) => {
     app.getRestrictedDb().find({}, function (err, docs) {
       res.json(docs);
